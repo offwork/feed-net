00363aa3d5941f00e08a8e12b963a5574b27a713
offwork <chomunist@gmail.com>
2020-08-20 13:14:32 +0300

    initial commit

---
+++ apps/movie-critic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feed-net-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'movie-critic';
+}
