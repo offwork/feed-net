8d173dec335aaca7594df1e26c87d2118afa685f
offwork <chomunist@gmail.com>
2020-09-06 14:50:52 +0300

    core http testing

--- libs/core/http/jest.config.js
+++ libs/core/http/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: 'core-http',
   preset: '../../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
